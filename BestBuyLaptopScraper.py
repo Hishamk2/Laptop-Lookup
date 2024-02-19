9cdec99e4fcee15b03f82b345c599232e59f667f
Hishamk <67921181+Hishamk2@users.noreply.github.com>
2024-02-18 21:07:58 -0600

    First Day of webscraping done. Mostly experimenting

--- BestBuyLaptopScraper.py
+++ BestBuyLaptopScraper.py
@@ -1,5 +1,25 @@
-print("Just testing github with VS Code")
+from bs4 import BeautifulSoup
+import requests
 
-print("Testing new thing")
+print("Start of the program")
 
-print("This is changed from github")
+
+URL = "https://www.dell.com/en-ca/search/laptop?r=37654,37789&p=1&ac=facetselect&t=Product"
+page = requests.get(URL)  # Gets the response code
+page_html = page.text  # Gets the HTML content of the page
+
+# TODO Trees or smthn is going on here, LEARN MORE ABT THIS
+# TODO Could have also used filehandle: with open(URL) as f: soup = BS(f, 'html.parser')
+# Parses the HTML content into a BeautifulSoup object
+soup = BeautifulSoup(page_html, "html.parser")
+
+# Doing soup.find("head") is same as doing soup.head
+print(soup.find(class_="ps-dell-price ps-simplified").text)
+
+
+print("End of the program")
+
+# "C:\Users\hamza\Documents\HISHAM\Computer Science\BestBuy Laptop Scraper"
+
+# i3 https://www.dell.com/en-ca/search/laptop?r=37654&p=1&ac=facetselect&t=Product
+# i5 https://www.dell.com/en-ca/search/laptop?r=37653&p=1&ac=facetselect&t=Product
