6c6d5ffb6b27db29b5c6228d238bec16f514d86e
Hishamk <67921181+Hishamk2@users.noreply.github.com>
2024-02-21 22:31:12 -0600

    Extracted all relevant data, get 403 error. Need to fix that.

--- DellLaptopScraper.py
+++ DellLaptopScraper.py
@@ -8,23 +8,31 @@ print("Start of the program")
 
 # TODO This link is for laptops sorted from low to high, shouldnt matter though
 
-URL = "https://www.dell.com/en-ca/search/laptop?p=1&r=37654,37789&t=Product"
-page = requests.get(URL)  # Gets the response code
-page_html = page.text  # Gets the HTML content of the page
+URL = """
+https://www.dell.com/en-ca/search/laptop?sb=pricing.saleprice,asc&p=1&t=Product&r=37654
+"""
+proxy = '124.156.219.100:80'
+header = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
 
+# Gets the response code
+# page = requests.get(URL, proxies={'http': proxy, 'https': proxy}, timeout=3)
+page = requests.get(URL, headers=header, timeout=3)
+page_html = page.text  # Gets the HTML content of the page
+print(page.status_code)
 # TODO Trees or smthn is going on here, LEARN MORE ABT THIS
 # TODO Could have also used filehandle: with open(URL) as f: soup = BS(f, 'html.parser')
-# Parses the HTML content into a BeautifulSoup object
+# Parses the HTML content into a BeautifulSoup object`  `
 soup = BeautifulSoup(page_html, "html.parser")
 
 # Doing soup.find("head") is same as doing soup.head
 # Gets the price of all the laptops, gets two of each for now
 
 # TODO Make this better, there has to be some way of doing x.y.z etc or smthn. This seems too crude
-# Gets the range of what results qare beiung shown on the current page
-result_range = soup.find(class_="pageinfo")
-# Gets the number of results that are being shown on teh current page
-num_results_page = result_range.find_next("label").text[-1]
+# # Gets the range of what results qare beiung shown on the current page
+# result_range = soup.find(class_="pageinfo")
+# # Gets the number of results that are being shown on teh current page
+# num_results_page = result_range.find_next("label").text[-1]
 
 laptop_price = []
 laptop_price_html = []
@@ -44,6 +52,7 @@ laptop_name_html = soup.findAll(class_="ps-title")
 for name in laptop_name_html:
     name_text = name.text
     laptop_name.append(name_text)
+    print(name_text)
 
 
 laptop_processor = []
@@ -65,11 +74,16 @@ for ram in laptop_ram_html:
         class_="ps-iconography-specs-label")[-3].text.strip()
     laptop_ram.append(ram_text)
 
-for i in range(len(laptop_name_html)):
-    print(laptop_name[i], end="")
-    print(laptop_price[i])
-    print(laptop_processor[i])
-    print(laptop_ram[i])
+print(len(laptop_name_html))
+print(len(laptop_price_html))
+print(len(laptop_processor_html))
+print(len(laptop_ram_html))
+
+# for i in range(len(laptop_name_html)):
+#     print(laptop_name[i], end="")
+#     print(laptop_price[i])
+#     print(laptop_processor[i])
+#     print(laptop_ram[i])
 
 # Try getting erht a tags instead
 
